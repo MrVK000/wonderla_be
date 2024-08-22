1a3a2ef6f53f5174de51961fcc424c6987dc581b
VK <vaikundamuthu@gmail.com>
2024-08-22 19:15:27 +0530

    implemented port configuration on server.js file

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ require('dotenv').config();  // Load environment variables
 const express = require('express');
 const app = express();
 const router = require('./router');
+const port = 8000 | process.env.DATABASE_URL;
 
 app.use(express.json());
 app.use('/', router);
 
-app.listen(8000, () => {
-    console.log("Server is listening....");
+app.listen(port, () => {
+    console.log(`Server is listening at ${port}>>>`);
 });
