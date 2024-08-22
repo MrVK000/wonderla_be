f6da55dfab42ce6dc3994acef7bc89884e0503af
VK <vaikundamuthu@gmail.com>
2024-08-22 19:18:58 +0530

    fix for port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config();  // Load environment variables
 const express = require('express');
 const app = express();
 const router = require('./router');
-const port = 8000 | process.env.DATABASE_URL;
+const port = 8000;
 
 app.use(express.json());
 app.use('/', router);
