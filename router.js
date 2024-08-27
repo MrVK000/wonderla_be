cb09a26ea283b8bcb50dc4281ef0c31b1f763fd9
VK <vaikundamuthu@gmail.com>
2024-08-27 16:28:32 +0530

    modified the router file

--- router.js
+++ router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
+
 const mobileMenuRouter = require("./src/mobileMenu");
-// const mobileMenuRouter = require("./src/mobileMenu");
 const liveChatConversationPropertyRouter = require("./src/liveChatConversationProperty");
 const outingRouter = require("./src/outing");
 const resortRouter = require("./src/resort");
@@ -11,6 +11,12 @@ const contactLinksRouter = require("./src/contactLinks");
 const addressRouter = require("./src/address");
 const socialLinksRouter = require("./src/socialLinks");
 
+// Root route
+router.get('/', (req, res) => {
+    res.send('Welcome to the API');
+});
+
+// API routes
 router.use('/mobileMenu', mobileMenuRouter);
 router.use('/liveChatConversationProperty', liveChatConversationPropertyRouter);
 router.use('/outing', outingRouter);
@@ -21,5 +27,9 @@ router.use('/contactLinks', contactLinksRouter);
 router.use('/address', addressRouter);
 router.use('/socialLinks', socialLinksRouter);
 
+// Fallback route for undefined paths
+router.use('*', (req, res) => {
+    res.status(404).send('API route not found');
+});
 
 module.exports = router;
